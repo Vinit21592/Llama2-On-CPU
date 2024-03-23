1c9d3e6f53e735bf807efb0cc5a09c6b3657e296
vinit <vinit.londhe21@gmail.com>
2024-03-23 19:53:02 +0530

    app added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+ 
+setup(
+    name= 'Llama2 On CPU',
+    version= "0.0.0",
+    author= "Vinit21592",
+    author_email= "vinit.londhe21@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+    )
